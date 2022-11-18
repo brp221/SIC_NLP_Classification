0a9f2815401902f50b13f1e9b4035769a9fffb8d
Bratislav Petkovic <brp221@lehigh.edu>
2022-11-17 20:59:30 -0500

    Ooh baby baby

--- IndustrySimilarities__NLP_Algo__.py
+++ IndustrySimilarities__NLP_Algo__.py
@@ -18,6 +18,8 @@ from nltk.corpus import stopwords
 from nltk import download
 import numpy as np
 industrySet = pd.read_csv("Z:/__Bratislav_Petkovic/RenewalGenius/Industries_Study/IndustryGrouping.csv")
+industrySet = pd.read_excel("Z:/__Bratislav_Petkovic/RenewalGenius/Industries_Study/IndustryGroupingFullDataset.xlsx")
+
 industrySet = industrySet.sort_values('SIC_INDUSTRY_CLASS_NM', ascending=True)
 industrySet_alpha = industrySet.set_index([pd.Index([*range(0,len(industrySet), 1) ])], 'SIC_INDUSTRY_CLASS_NM')
 
@@ -27,9 +29,10 @@ industrySet_alpha["sub_industry_filtered"] = [phrase.replace(',',"") for phrase 
 documents = list(industrySet_alpha.sub_industry_filtered)
 stoplist = set(['or', ',']) #'product', 'products', 'service', 'services', 'of'])
 stop_words = stopwords.words('english')
-stop_words_mine = [ 'goods','nec', '&', 'services', 'stores', 'equipment', 'products', 'services,', 'commercial', "agencies", 
-                   "preparation","prepare","agency", "products,", "other", "goods", "national", "protection", "protecting", "bodies",
-                   "plans" ,"mineral", "minerals"]
+stop_words_mine = [ 'goods','nec', '&', 'services', 'store', 'equipment', 'products', 'services,', 'commercial', "agencies", 
+                   "preparation","preparations","prepare","agency", "products,", "other", "goods", "national", "protection", "protecting", "bodies",
+                   "plans" ,"mineral", "minerals", "misc.", "miscellaneous", "clinics", "specialty", "special", "mills", "specialties", "exc.", "ex."
+                   "supply","supplies", "stores" ,"system" ,"systems", "allied", "public", "primary", "facilities", "centers", "related", "shops"]
 stop_words_new = list(set(stop_words + stop_words_mine))
 texts = [  [word.lower() for word in document.split() if word.lower() not in stop_words_new]  for document in documents]
 
@@ -40,11 +43,11 @@ industrySet_alpha["sub_industry_filtered"] = [' '.join(word_arr) for word_arr in
 
 import tensorflow_hub as hub
 from scipy.spatial import distance
-embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")  
+embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")  # delet folder in file explorer if (OS) error 
 
 def get_similar_industries_ten_flow(given_industry, industry_df, similarity_threshold= 0.0, pool_threshold = 10):
     
-    data = {'ComparerIndustry':[],'ComparerSubIndustry': [],'Similarity': []}
+    data = {'ComparerSubIndustry': [],'Similarity': []}
     processed_sub_ind = industry_df[industry_df["SIC_INDUSTRY_CLASS_NM"] == given_industry]["sub_industry_filtered"].iloc[0]
     # print(processed_sub_ind)
     for index, row in industry_df.iterrows():
@@ -52,8 +55,7 @@ def get_similar_industries_ten_flow(given_industry, industry_df, similarity_thre
         # print(row.Industry)
         embeddings = embed([processed_sub_ind, row.sub_industry_filtered])
         data["ComparerSubIndustry"].append(row.SIC_INDUSTRY_CLASS_NM)
-        data["ComparerIndustry"].append(row.Industry)
-
+        # data["ComparerIndustry"].append(row.Industry)
         data["Similarity"].append(1 - distance.cosine(embeddings[0], embeddings[1]))
  
     resultDF = pd.DataFrame(data)
@@ -104,14 +106,14 @@ termsim_matrix = SparseTermSimilarityMatrix(termsim_index, dictionary, tfidf)
 
 def get_similar_industries_gen_sim(given_industry, industry_df):
     sic_industry_index = industry_df.index[industry_df['SIC_INDUSTRY_CLASS_NM'] == given_industry].tolist()[0]
-    data = {'ComparerIndustry':[], 'ComparerSubIndustry': [],'Similarity': []}
+    data = {'ComparerSubIndustry': [],'Similarity': []}
     for i in list(industry_df.SIC_INDUSTRY_CLASS_NM):
         comparer_industry_index = industry_df.index[industry_df['SIC_INDUSTRY_CLASS_NM'] == i].tolist()[0]
         similarity = termsim_matrix.inner_product(tfidf_phrases[sic_industry_index], tfidf_phrases[comparer_industry_index], normalized=(True, True))
         # print('similarity = %.4f' % similarity)
         data['ComparerSubIndustry'].append(i)
         data['Similarity'].append(similarity)
-        data["ComparerIndustry"].append(industry_df[industry_df["SIC_INDUSTRY_CLASS_NM"] == given_industry]["Industry"].iloc[0])
+        # data["ComparerIndustry"].append(industry_df[industry_df["SIC_INDUSTRY_CLASS_NM"] == given_industry]["Industry"].iloc[0])
         
     gen_sim_result_df = pd.DataFrame(data)
     gen_sim_result_df = gen_sim_result_df[gen_sim_result_df.Similarity > 0]
@@ -132,7 +134,8 @@ beer_and_ale = get_similar_industries_gen_sim("Beer and ale", industrySet_alpha)
 
 # time complexity == O(n*(2*n) == 2n^2), 
 # returns a dictionary of dataframes where keys are industries and values are dataframe results
-def get_all_similar_industries(industry_df, algo_weights=[0.75,0.25]):
+def get_all_similar_industries(industry_df, algo_weights=[0.5,0.5]):
+    full_df = pd.DataFrame()
     df_dict = {}
     counter = 0
     for sub_industry in list(industry_df.SIC_INDUSTRY_CLASS_NM): 
@@ -151,18 +154,19 @@ def get_all_similar_industries(industry_df, algo_weights=[0.75,0.25]):
             print(" I am here uh oh")
             # do a left merge on each and see which one is better ? 
         join_results = join_results.replace(np.nan, 0)
-        join_results["ComparerIndustry"] = np.where(join_results.ComparerIndustry__GS__.notnull(),join_results.ComparerIndustry__GS__, join_results.ComparerIndustry__TF__ )
+        join_results["SIC_Industry"] = sub_industry
+        # join_results["ComparerIndustry"] = np.where(join_results.ComparerIndustry__GS__.notnull(),join_results.ComparerIndustry__GS__, join_results.ComparerIndustry__TF__ )
         join_results["Combo_Similarity"] = join_results['Similarity__GS__'] * algo_weights[0] + join_results['Similarity__TF__'] * algo_weights[1]
         
-        join_results = join_results[['ComparerSubIndustry', 'Similarity__GS__', 'Similarity__TF__', 'ComparerIndustry', 'Combo_Similarity']]
+        join_results = join_results[['SIC_Industry','ComparerSubIndustry', 'Similarity__GS__', 'Similarity__TF__', 'Combo_Similarity']]
         join_results = join_results.sort_values('Combo_Similarity', ascending=False)
 
         df_dict[sub_industry] = join_results
-        
-        # counter+=1
+        full_df = pd.concat([full_df, join_results], ignore_index = True)
+        counter+=1
         # if(counter>10):
         #     break
-        
+    full_df.to_csv("Z:/__Bratislav_Petkovic/RenewalGenius/Industries_Study/NLP_Results.csv",index=False)
     return df_dict
 
 
